641f19dd40102b5daa2995170fd9901b4989d396
SunmeetOberoi <sunmeet.oberoi@gmail.com>
2020-03-21 18:17:18 +0530

    added registeration functionalty

--- Protal/app/src/main/java/com/protal/LoginFragment.java
+++ Protal/app/src/main/java/com/protal/LoginFragment.java
@@ -1,8 +1,5 @@
 package com.protal;
 
-import androidx.lifecycle.ViewModelProviders;
-
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -18,14 +15,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.concurrent.Executor;
 
 public class LoginFragment extends Fragment {
 
@@ -39,7 +33,7 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.login_fragment, container, false);
+        final View view = inflater.inflate(R.layout.fragment_login, container, false);
         final EditText etEMailLogin = view.findViewById(R.id.etEMailLogin);
         final EditText etPasswordLogin = view.findViewById(R.id.etPasswordLogin);
         Button btnLoginSubmit = view.findViewById(R.id.btnLoginSubmit);
@@ -51,12 +45,10 @@ public class LoginFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(etEMailLogin.getText().toString().trim().isEmpty()) {
-                    tvLoginError.setText(R.string.empty_email_error);
-                    tvLoginError.setVisibility(View.VISIBLE);
+                    setErrorMessage(getResources().getString(R.string.empty_email_error), true);
                 }
                 else if(etPasswordLogin.getText().toString().trim().isEmpty()) {
-                    tvLoginError.setText(R.string.empty_password_error);
-                    tvLoginError.setVisibility(View.VISIBLE);
+                    setErrorMessage(getResources().getString(R.string.empty_password_error), true);
                 }
                 else
                     login(etEMailLogin.getText().toString().trim(),
@@ -68,17 +60,14 @@ public class LoginFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(etEMailLogin.getText().toString().trim().isEmpty()) {
-                    tvLoginError.setText(R.string.empty_email_error);
-                    tvLoginError.setVisibility(View.VISIBLE);
+                    setErrorMessage(getResources().getString(R.string.empty_email_error), true);
                 }
                 else {
                     FirebaseAuth.getInstance().sendPasswordResetEmail(etEMailLogin.getText()
                             .toString().trim()).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
                         public void onSuccess(Void aVoid) {
-                            tvLoginError.setText(R.string.forgot_pass_success);
-                            tvLoginError.setTextColor(getResources().getColor(R.color.colorPrimary));
-                            tvLoginError.setVisibility(View.VISIBLE);
+                            setErrorMessage(getResources().getString(R.string.forgot_pass_success), false);
                         }
                     });
                 }
@@ -94,19 +83,24 @@ public class LoginFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            //TODO: Login
+                            //TODO: Proceed to main homepage
                             Toast.makeText(getActivity(), "Success", Toast.LENGTH_SHORT).show();
                             pbLoadingLogin.setVisibility(View.GONE);
                             tvLoginError.setVisibility(View.GONE);
                             pbLoadingLogin.setVisibility(View.GONE);
                         }
                         else{
-                            tvLoginError.setText(task.getException().getLocalizedMessage());
-                            tvLoginError.setVisibility(View.VISIBLE);
+                            setErrorMessage(task.getException().getLocalizedMessage(), true);
                         }
                     }
                 });
     }
 
-
+    void setErrorMessage(String message, Boolean error){
+        tvLoginError.setText(message);
+        if(!error) {
+            tvLoginError.setTextColor(getResources().getColor(R.color.colorPrimary));
+        }
+        tvLoginError.setVisibility(View.VISIBLE);
+    }
 }
--- Protal/app/src/main/java/com/protal/RegisterFragment.java
+++ Protal/app/src/main/java/com/protal/RegisterFragment.java
@@ -1,15 +1,36 @@
 package com.protal;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.LongDef;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 public class RegisterFragment extends Fragment {
+
+
+    ProgressBar pbLoadingRegister;
+    TextView tvRegisterError;
+    Button btnRegisterSubmit;
+
 
     public static RegisterFragment newInstance() {
         return new RegisterFragment();
@@ -20,6 +41,80 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View view =  inflater.inflate(R.layout.fragment_register, container, false);
+        final EditText etNameRegister = view.findViewById(R.id.etNameRegister);
+        final EditText etEMailRegister = view.findViewById(R.id.etEMailRegister);
+        final EditText etPasswordRegister = view.findViewById(R.id.etPasswordRegister);
+        final EditText etPasswordConfirmRegister = view.findViewById(R.id.etPasswordConfirmRegister);
+        btnRegisterSubmit = view.findViewById(R.id.btnRegisterSubmit);
+        tvRegisterError = view.findViewById(R.id.tvRegisterError);
+        pbLoadingRegister = view.findViewById(R.id.pbLoadingRegister);
+
+        btnRegisterSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // prevent resubmission of form
+                btnRegisterSubmit.setClickable(false);
+                // field validation
+                if(etNameRegister.getText().toString().trim().isEmpty())
+                    setErrorMessage(getResources().getString(R.string.empty_name_error), true);
+                else if(etEMailRegister.getText().toString().trim().isEmpty())
+                    setErrorMessage(getResources().getString(R.string.empty_email_error), true);
+                else if(etPasswordRegister.getText().toString().isEmpty())
+                    setErrorMessage(getResources().getString(R.string.empty_password_error), true);
+                else if(etPasswordConfirmRegister.getText().toString().isEmpty())
+                    setErrorMessage(getResources().getString(R.string.empty_confirm_password_error)
+                            , true);
+                else if(!etPasswordRegister.getText().toString()
+                        .equals(etPasswordConfirmRegister.getText().toString()))
+                    setErrorMessage(getResources().getString(R.string.password_diff_error), true);
+                else{
+                    register_user(etNameRegister.getText().toString().trim(),
+                            etEMailRegister.getText().toString().trim(),
+                            etPasswordRegister.getText().toString().trim());
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void register_user(final String name, String email, String password) {
+        pbLoadingRegister.setVisibility(View.VISIBLE);
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull final Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //TODO: procees to main homepage
+                            task.getResult().getUser().updateProfile(new UserProfileChangeRequest
+                                    .Builder()
+                            .setDisplayName(name)
+                            .build()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> taskresult) {
+                                    // verify email
+                                    task.getResult().getUser().sendEmailVerification();
+                                    setErrorMessage(getString(R.string.confirm_email_message), false);
+                                    pbLoadingRegister.setVisibility(View.GONE);
+                                }
+                            });
+                        }else{
+                            setErrorMessage(task.getException().getLocalizedMessage(), true);
+                            pbLoadingRegister.setVisibility(View.GONE);
+
+                        }
+                    }
+                });
+    }
+
+    // handles displaying message
+    private void setErrorMessage(String message, Boolean error){
+        tvRegisterError.setText(message);
+        if(!error) {
+            tvRegisterError.setTextColor(getResources().getColor(R.color.colorPrimary));
+        }
+        tvRegisterError.setVisibility(View.VISIBLE);
+        btnRegisterSubmit.setClickable(true);
     }
 }
